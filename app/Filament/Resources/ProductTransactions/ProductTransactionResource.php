c87ffe3531f8be662177993dec95b3114f99193f
Dirga36 <155790299+Dirga36@users.noreply.github.com>
2026-01-22 14:47:01 +0000

    Apply automatic changes

--- app/Filament/Resources/ProductTransactions/ProductTransactionResource.php
+++ app/Filament/Resources/ProductTransactions/ProductTransactionResource.php
@@ -15,7 +15,8 @@ use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-//use UnitEnum;
+
+// use UnitEnum;
 
 class ProductTransactionResource extends Resource
 {
