8025433ea4253c97d90795dd9869f3c0022b67af
Dirga36 <155790299+Dirga36@users.noreply.github.com>
2026-02-02 13:14:57 +0000

    Apply automatic changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Brand;
 use App\Models\Category;
-use App\Models\ProductTransaction;
 use App\Models\Produk;
 use App\Models\PromoCode;
 use App\Models\User;
