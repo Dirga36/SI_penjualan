35573684b06432e63d1d69fc311f2aacade708b8
Dirga36 <155790299+Dirga36@users.noreply.github.com>
2026-02-02 13:40:53 +0000

    Apply automatic changes

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -23,7 +23,7 @@ class UsersTable
                 TextColumn::make('email')
                     ->label('Email address')
                     ->searchable(),
-                //TextColumn::make('email_verified_at')
+                // TextColumn::make('email_verified_at')
                 //    ->dateTime()
                 //    ->sortable(),
                 TextColumn::make('created_at')
