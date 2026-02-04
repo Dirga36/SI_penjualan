f724c91c7ad9d28e9b7c98b795e9e354b44f4ed7
Dirga36 <155790299+Dirga36@users.noreply.github.com>
2026-02-04 11:21:10 +0000

    Apply automatic changes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers\Filament;
 
-use Awcodes\Overlook\Widgets\OverlookWidget;
 use Awcodes\Overlook\OverlookPlugin;
+use Awcodes\Overlook\Widgets\OverlookWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
